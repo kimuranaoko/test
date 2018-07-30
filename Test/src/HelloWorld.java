7cf28b3c4c67f1e7bfb33faedd76903b21e7c625
kimuranaoko <knao1217@gmail.com>
2018-07-30 13:03:11 +0900

    20180730

---
+++ Test/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("Hello World");
+
+		String s="Hello";
+		System.out.println(s);
+
+		String a=new String("Hello");
+		System.out.println(a);
+	}
+
+}
---
+++ TestDB/sql/test2db.sql
@@ -0,0 +1,5 @@
+drop database if exists test2db;
+create datatable test2db;
+use test2db;
+
+create table
